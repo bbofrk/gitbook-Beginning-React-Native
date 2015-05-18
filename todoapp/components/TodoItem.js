b33a988f5806feb5bc0d995524a27332f4467114
bbofrk <mingliwanglau@gmail.com>
2015-05-19 08:08:50 +1200

    react

---
+++ todoapp/components/TodoItem.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var React = require('react');
+var TodoItem = React.createClass({
+  getInitialState: function(){
+    return {edit: false, content: this.props.item.content}
+  },
+  render: function(){
+    if (this.state.edit === false) {
+      return (
+        <li>
+          {this.props.item.content}&nbsp;<button onClick={() => this.setState({edit: true})}>Edit</button>
+        </li>
+      )
+    } else {
+      return (
+        <li>
+          <input type="text" value={this.state.content} onChange={(event) => this.setState{content: event.tartget.value}} />
+        </li>
+      )
+    }
+  }
+});
+module.exports = TodoItem;
