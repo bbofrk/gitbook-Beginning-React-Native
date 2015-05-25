3d736f21843299b01b85b808f9e70b6d6585aa27
bbofrk <mingliwanglau@gmail.com>
2015-05-26 07:42:31 +1200

    express server

---
+++ todoserver/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html')
+});
+app.get('/todos', function(req, res) {
+  var todos = ['todo item1', 'todo item 2', 'todo item 3'];
+  res.send(todos);
+});
+app.listen(3000, function(){
+  console.log('Listen on port 3000');
+});
